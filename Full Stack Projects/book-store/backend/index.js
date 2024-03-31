d6ba79dee4018fc482a09ef4efc5187c2193be8a
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-03-31 22:18:33 +0530

    Routers Added

--- Full Stack Projects/book-store/backend/index.js
+++ Full Stack Projects/book-store/backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from './config.js'
 import booksRoute from "./routes/booksRoute.js";
 import mongoose from "mongoose";
+import cors from 'cors'
 
 const app = express();
 
@@ -10,14 +11,14 @@ app.use(express.json())
 app.use("/books",booksRoute)
 
 // Allow users of all configurations
-//app.use(cors())
+app.use(cors())
 
 // Allow users of custom origin
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ['GET','POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type']
-}));
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: ['GET','POST','PUT','DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }));
 
 app.get("/", (req, res) => {
     return res.status(200).send("Welcome to Book Store App !")
--- Full Stack Projects/book-store/frontend/src/App.jsx
+++ Full Stack Projects/book-store/frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
+      <Route path='/books/details/:id' element={<ShowBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+    </Routes>
   )
 }
 
